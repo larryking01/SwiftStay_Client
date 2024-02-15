00fd3c015985d448dc05b80bf4aef195bebce9ec
larry king <larryking8118@gmail.com>
2024-02-15 15:29:59 +0000

    refactoring newsletter and contact-us

--- src/Routes/ContactUsHelp.js
+++ src/Routes/ContactUsHelp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Button from 'react-bootstrap/Button'
@@ -27,6 +27,59 @@ const ContactUsHelp = ( ) => {
           behavior: 'smooth'
         })
     }, [ ])
+
+
+    // handling state.
+    const [ customerEmail, setCustomerEmail ] = useState('')
+    const [ customerName, setCustomerName ] = useState('')
+    const [ subject, setSubject ] = useState('')
+    const [ description, setDescription ] = useState('')
+    const [ submitComplaintError, setSubmitComplaintError ] = useState('')
+
+
+    const UpdateCustomerEmail = ( event ) => {
+        setCustomerEmail( event.target.value )
+    }
+
+    const UpdateCustomerName = ( event ) => {
+        setCustomerName( event.target.value )
+    }
+
+    const UpdateSubject = ( event ) => {
+        setSubject( event.target.value )
+    }
+
+    const UpdateDescription = ( event ) => {
+        setDescription( event.target.value )
+    }
+
+
+    const HandleComplaintSubmit = ( ) => {
+
+        let regex = /^(?:(?:[^<>()[\].,;:\s@\"]+(?:\.[^<>()[\].,;:\s@\"]+)*)|(\".+\"))@(?:(?:\[(?:[0-9]{1,3}\.){3}[0-9]{1,3}\])|(?:(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}))$/
+        
+        if( customerEmail.length === 0 ) {
+            setSubmitComplaintError('No email provided. Email is required')
+        }
+        else if( customerName.length === 0 ) {
+            setSubmitComplaintError('No name provided. Name is a required field')
+        }
+        else if( subject.length === 0 ) {
+            setSubmitComplaintError('No subject provided. Subject is a required field')
+        }
+        else if( description.length === 0 ) {
+            setSubmitComplaintError('No description provided. Description is a required field')
+        }
+        else if( !customerEmail.match( regex ) ) {
+            setSubmitComplaintError('Invalid email. Provide a valid email to continue')
+        }
+        else {
+            setSubmitComplaintError('')
+            alert('Your complaint has been submitted successfully')
+        }
+    }
+
+
       
     return (
         <div>
@@ -43,17 +96,21 @@ const ContactUsHelp = ( ) => {
                 <section className='contact-us-details-section'>
                     <Form.Group className='mb-4'>
                         <Form.Label>Your email address * </Form.Label>
-                        <Form.Control type='email' placeholder='' className='contact-us-textbox text-control-focus-style' />
+                        <Form.Control type='email' placeholder='' className='contact-us-textbox text-control-focus-style'
+                                      onChange={ UpdateCustomerEmail } value={ customerEmail } />
                     </Form.Group>
 
                     <Form.Group className='mb-4'>
                         <Form.Label>Your name *</Form.Label>
-                        <Form.Control type='text' placeholder='' className='contact-us-textbox text-control-focus-style' />
+                        <Form.Control type='text' placeholder='' className='contact-us-textbox text-control-focus-style'
+                                      onChange={ UpdateCustomerName } value={ customerName } />
                     </Form.Group>
 
                     <Form.Group className='mb-5'>
                         <Form.Label>What can we help you with? *</Form.Label>
-                        <Form.Select aria-label='help subject select' className='contact-us-textbox text-control-focus-style'>
+                        <Form.Select aria-label='help subject select' 
+                                     className='contact-us-textbox text-control-focus-style'
+                                     onChange={ UpdateSubject } value={ subject }>
                             <option>-- Select subject --</option>
                             <option value='An existing booking'>An existing booking</option>
                             <option value='Searching on our website'>Searching on our website</option>
@@ -64,14 +121,17 @@ const ContactUsHelp = ( ) => {
                     </Form.Group>
 
                     <FloatingLabel controlId='floatingTextarea' label='Description *' className='mb-4'>
-                        <Form.Control as='textarea' placeholder='' className='description-text-area text-control-focus-style' style={{ height: 150 }} />
+                        <Form.Control as='textarea' placeholder='' className='description-text-area text-control-focus-style' 
+                                      style={{ height: 150 }} onChange={ UpdateDescription } value={ description } />
                     </FloatingLabel>
 
-                    <div className='attach-files-div mb-4'>
-                        <p>Attach file or drop files here</p>
-                    </div>
+                    <p className='submit-complaint-error-text'>{ submitComplaintError }</p>
 
-                    <Button variant='custom' className='contact-us-btn'> Submit </Button>
+                    {/* <div className='attach-files-div mb-4'>
+                        <p>Attach file or drop files here</p>
+                    </div> */}
+
+                    <Button variant='custom' className='contact-us-btn' onClick={ HandleComplaintSubmit }> Submit </Button>
 
                 </section>
 

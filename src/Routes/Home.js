d4a8cbcc960340b560ca0f6b7e07a9fada7ceb31
larry king <larryking8118@gmail.com>
2024-02-26 13:48:12 +0000

    refactoring landing page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -229,7 +229,7 @@ const Home = () => {
   let catchPhrasesArray = [
     'Check-in to comfort, check-out with memories. Your next unforgettable stay awaits,',
     'Step into luxury, book your stay today – where comfort meets elegance, and every stay is a dream come true!',
-    'Two words, endless comfort. Embrace the extraordinary at SwiftStay, where every reservation is a journey of delight.',
+    "Two words, 'endless comfort !'. Embrace the extraordinary at SwiftStay, where every reservation is a journey of delight.",
     'A world of luxury awaits behind our double doors. Secure your stay and indulge our elegance.',
     'Welcome to a symphony of comfort and style. Your key to an unforgettable stay is just a click away – book now at SwiftStay.'
   ]
